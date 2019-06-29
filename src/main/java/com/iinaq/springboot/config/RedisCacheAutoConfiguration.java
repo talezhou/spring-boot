a60df340f8d83beea4c4326a0cc03477c348bb97
iinaq <302506562@qq.com>
2019-06-29 17:32:00 +0800

    commit

---
+++ src/main/java/com/iinaq/springboot/config/RedisCacheAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.iinaq.springboot.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class RedisCacheAutoConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
---
+++ src/test/java/com/iinaq/springboot/RedisTest.java
@@ -0,0 +1,39 @@
+package com.iinaq.springboot;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class RedisTest extends ChapterApplicationTests{
+
+    private static final Logger log = LoggerFactory.getLogger(RedisTest.class);
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, Serializable> redisCacheTemplate;
+
+    @Test
+    public void get() {
+        // TODO 测试线程安全
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+        IntStream.range(0, 1000).forEach(i ->
+                executorService.execute(() -> stringRedisTemplate.opsForValue().increment("kk", 1))
+        );
+        stringRedisTemplate.opsForValue().set("k1", "v1");
+        final String k1 = stringRedisTemplate.opsForValue().get("k1");
+        log.info("[字符缓存结果] - [{}]", k1);
+        // TODO 以下只演示整合，具体Redis命令可以参考官方文档，Spring Data Redis 只是改了个名字而已，Redis支持的命令它都支持
+        String key = "battcn:user:1";
+        redisCacheTemplate.opsForValue().set(key, new User(1L, "u1", "pa"));
+        // TODO 对应 String（字符串）
+        final User user = (User) redisCacheTemplate.opsForValue().get(key);
+        log.info("[对象缓存结果] - [{}]", user);
+    }
+}
