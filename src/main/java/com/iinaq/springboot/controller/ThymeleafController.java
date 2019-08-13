b21e82eae20b5b34000e7fc92272732bbd1c9274
iinaq <302506562@qq.com>
2019-08-13 21:04:54 +0800

    commit

--- src/main/java/com/iinaq/springboot/controller/ThymeleafController.java
+++ src/main/java/com/iinaq/springboot/controller/ThymeleafController.java
@@ -3,7 +3,7 @@ package com.iinaq.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
